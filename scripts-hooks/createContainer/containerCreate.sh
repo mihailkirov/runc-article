b03ab6d7198b278c2982285771292fb31f479fb2
cryptonite <mihailkirovv@gmail.com>
2022-04-19 23:48:39 +0200

    Fixed bugs in transform.py; Problem with hangup of the server is still present;

--- scripts-hooks/createContainer/containerCreate.sh
+++ scripts-hooks/createContainer/containerCreate.sh
@@ -1,15 +1,12 @@
-#!/bin/sh
-
 # containerCreate script - test env config
 # executes in the container namespace before being chrooted
 
 SAVETO="./home/write-results/createContainer.txt";
 # test network interface connectivity
-ping -c 1 172.12.0.11;
 # check that the next hook script is available
-stat /home/tmpfssc;
+ping -c 1 172.12.0.11 && stat /home/tmpfssc;
 # give us some info
-echo "CREATE CONTAINER" > $SAVETO;
+echo "$0" > $SAVETO;
 for file in /proc/self/ns/*; do
 	readlink $file >> $SAVETO; 
 done
--- scripts-hooks/poststart/postStart.sh
+++ scripts-hooks/poststart/postStart.sh
@@ -1,14 +1,9 @@
-#!/bin/sh
-
 # postStart script - to indicate if the env config and test phase went okey
 # and trigger post-setup actions on the host
 # executed in the runtime namespace without chroot
-
 SAVETO="results-scripts/postStart.txt";
 # starts a small python3 server
-
-python3 $CDIR/scripts-hooks/httph.py; # goes in the background
-
+python3 $CDIR/httph.py; # goes in the background
 # give us some info
 echo "$0" > $CDIR/$SAVETO;
 for file in /proc/self/ns/*; do
